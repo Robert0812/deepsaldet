efbc5e2b997ea00d2f4d38a24aa59fbbb770ea69
Rui Zhao <fantasticzr@gmail.com>
2015-04-29 22:37:32 +0000

    model downloader

---
+++ models/get_models.sh
@@ -0,0 +1,6 @@
+echo "Downloading models:"
+echo ""
+wget www.ee.cuhk.edu.hk/~rzhao/sharedata/model_imagenet_bbox_pretrain_iter_780000_cascade_stage2_iter_80000.caffemodel
+wget www.ee.cuhk.edu.hk/~rzhao/sharedata/model_imagenet_padimage_pretrain_iter_600000_stage1_iter_80000.caffemodel
+echo ""
+echo "Done."
