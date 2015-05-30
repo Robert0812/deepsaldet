a166f87f4c2d9b018dd14a540ef537a3947ed8ae
Rui Zhao <fantasticzr@gmail.com>
2015-05-30 10:57:26 +0000

    Update get_models.sh

--- models/get_models.sh
+++ models/get_models.sh
@@ -1,6 +1,8 @@
 echo "Downloading models:"
 echo ""
-wget www.ee.cuhk.edu.hk/~rzhao/sharedata/model_imagenet_bbox_pretrain_iter_780000_cascade_stage2_iter_80000.caffemodel
-wget www.ee.cuhk.edu.hk/~rzhao/sharedata/model_imagenet_padimage_pretrain_iter_600000_stage1_iter_80000.caffemodel
+#wget www.ee.cuhk.edu.hk/~rzhao/sharedata/model_imagenet_bbox_pretrain_iter_780000_cascade_stage2_iter_80000.caffemodel
+#wget www.ee.cuhk.edu.hk/~rzhao/sharedata/model_imagenet_padimage_pretrain_iter_600000_stage1_iter_80000.caffemodel
+wget https://www.dropbox.com/s/qb56hej372vxt8w/model_imagenet_bbox_pretrain_iter_780000_cascade_stage2_iter_80000.caffemodel
+wget https://www.dropbox.com/s/0rta2q1k50vmdoz/model_imagenet_padimage_pretrain_iter_600000_stage1_iter_80000.caffemodel
 echo ""
 echo "Done."
